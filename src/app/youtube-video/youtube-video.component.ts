b9bde9d41a6115734b936de7caaf1be7f5d18067
Adrien VERGNAUD <a.vergnaud@cat-amania.com>
2024-09-18 16:02:07 +0200

    ko

--- src/app/youtube-video/youtube-video.component.ts
+++ src/app/youtube-video/youtube-video.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 
 @Component({
@@ -8,18 +8,14 @@ import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
   templateUrl: './youtube-video.component.html',
   styleUrl: './youtube-video.component.css'
 })
-export class YoutubeVideoComponent {
+export class YoutubeVideoComponent implements OnInit {
  
   videoUrlString:string = "https://www.youtube.com/embed/dQw4w9WgXcQ";
   videoUrl:SafeResourceUrl;
 
-  constructor(private sanitizer:DomSanitizer) {
-    //debugger
-    if (window.UC_UI && window.UC_UI.isInitialized()) {
-      console.log('UC UI is already initialized');
-    } else {
-      console.log('UC UI is not initialized');
-    }
+  constructor(private sanitizer:DomSanitizer) { }
+
+  ngOnInit() {
     this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoUrlString);
   }
 }
