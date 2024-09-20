34b6f3b2178041a894efa81ac55b0a5e2aea4cf1
Adrien VERGNAUD <a.vergnaud@cat-amania.com>
2024-09-20 18:20:36 +0200

    security

--- src/app/youtube-video/youtube-video.component.ts
+++ src/app/youtube-video/youtube-video.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-youtube-video',
@@ -10,20 +10,22 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class YoutubeVideoComponent implements OnInit{
 
   @Input({ required: true }) videoId: string;
-  private readonly sanitizer= Inject(DomSanitizer);
-  trustedHtml: string;
+  trustedHtml: SafeHtml;
 
+  constructor(private sanitizer: DomSanitizer) {}
 
+  /* seule solution trouvée pour que usercentrics gère les iframes dans les routes */
   ngOnInit() {
-    const unsafeHtml = `<iframe 
+    let unsafeHtml:string = 'videoId must match the expected pattern';
+    if(/^[A-Za-z0-9_\-]{11}$/.test(this.videoId)) {
+      unsafeHtml = `<iframe 
   width="560" 
   height="315" 
   uc-src="https://www.youtube.com/embed/${this.videoId}"
   frameborder="0" 
   allowfullscreen>
 </iframe>`;
-
+    }
     this.trustedHtml = this.sanitizer.bypassSecurityTrustHtml(unsafeHtml);
   }
-
 }
